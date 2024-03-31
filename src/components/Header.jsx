ffcd5dc548bdb61db360b5ec3e2aec9c89e8ecce
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-03-31 13:50:06 +0530

    toggle bar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUserPlus,
@@ -9,39 +9,55 @@ import "../css/header.css";
 import logo from "../images/1.png";
 
 function Header() {
+  const [isToggle, setToggle] = useState(false);
+
+  const toggleMenu = () => {
+    setToggle(!isToggle);
+  };
+
   return (
-    <>
-      <header className="header">
-        <nav className="nav_bar">
-          <div className="brand">
-            <div className="brand_logo">
-              <img src={logo} alt="logo" />
-            </div>
-            <p className="bname disable-selection">CashewNuts</p>
+    <header className="header">
+      <nav className="nav_bar">
+        <div className="brand">
+          <div className="brand_logo">
+            <img src={logo} alt="logo" />
           </div>
-          <ul className="options nav_option disable-selection">
-            <li className="menu_option">
-              <div className="icon">
-                <FontAwesomeIcon icon={faHomeAlt} />
-              </div>
-              <p className="text disable-selection">Home</p>
-            </li>
-            <li className="menu_option">
-              <div className="icon">
-                <FontAwesomeIcon icon={faCartShopping} />
-              </div>
-              <p className="text disable-selection">Cart</p>
-            </li>
-            <li className="menu_option">
-              <div className="icon">
-                <FontAwesomeIcon icon={faUserPlus} />
-              </div>
-              <p className="text disable-selection">Login</p>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+          <p className="bname disable-selection">CashewNuts</p>
+        </div>
+        <div
+          className={`toggle_bar ${isToggle ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <div className="line top"></div>
+          <div className="line mid"></div>
+          <div className="line bot"></div>
+        </div>
+        <ul
+          className={`options nav_option ${
+            isToggle ? "active" : ""
+          } disable-selection`}
+        >
+          <li className="menu_option">
+            <div className="icon">
+              <FontAwesomeIcon icon={faHomeAlt} />
+            </div>
+            <p className="text disable-selection">Home</p>
+          </li>
+          <li className="menu_option">
+            <div className="icon">
+              <FontAwesomeIcon icon={faCartShopping} />
+            </div>
+            <p className="text disable-selection">Cart</p>
+          </li>
+          <li className="menu_option">
+            <div className="icon">
+              <FontAwesomeIcon icon={faUserPlus} />
+            </div>
+            <p className="text disable-selection">Login</p>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
