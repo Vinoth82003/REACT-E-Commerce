a24c354cf584ff8e9a059752211aefc7dcf81cc1
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-04-01 19:33:39 +0530

    login page simple html structure

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,20 @@
 import React from "react";
-import "../css/login.css";
 import img1 from "../illus/buy best product.png";
-import img2 from "../illus/best_seller.png";
-import img3 from "../illus/secure payment.png";
-import img4 from "../illus/Secure data.png";
+// import img2 from "../illus/best_seller.png";
+// import img3 from "../illus/secure payment.png";
+// import img4 from "../illus/Secure data.png";
+import "../css/login.css";
+
 const Login = () => {
   return (
     <>
       <div className="login_page">
-        <div className="inner_page">
-          <div className="left_images">
-            <div className="inner_image1">
-              <img src={img1} alt="left side show" />
-            </div>
-            <div className="inner_image1">
-              <img src={img2} alt="left side show" />
-            </div>
-            <div className="inner_image1">
-              <img src={img3} alt="left side show" />
-            </div>
-            <div className="inner_image1">
-              <img src={img4} alt="left side show" />
-            </div>
-          </div>
+        <div className="images">
+          <img src={img1} alt="show" />
+        </div>
+        <div className="inner_login">
+          <div className="left_images"></div>
+          <div className="login_page"></div>
         </div>
       </div>
     </>
