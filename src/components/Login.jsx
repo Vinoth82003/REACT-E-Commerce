ed8f50ee999f39fca961a03429b7d25c54158268
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-04-03 17:53:16 +0530

    login page js

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,8 @@ import {
   faUnlockKeyhole,
   faUserAlt,
   faUserCheck,
+  faExclamationTriangle,
+  faTimesCircle,
 } from "@fortawesome/free-solid-svg-icons";
 
 import img0 from "../illus/Cash Payment.png";
@@ -22,69 +24,95 @@ import "../css/login.css";
 const Login = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const loginDetails = useContext(loginContext);
+  const [isTermsCondition, setTermsCondition] = useState(true);
   useEffect(() => {
     const intervalId = setInterval(() => {
       setActiveIndex((prevIndex) => (prevIndex + 1) % 9); // Assuming you have 9 images
-    }, 2000); // Adjust the interval time as needed
+    }, 5000); // Adjust the interval time as needed
 
     return () => clearInterval(intervalId);
   }, []);
+
+  const Alert = () => {
+    return (
+      <>
+        <div className="alert">
+          <div className="alertClose">
+            <FontAwesomeIcon icon={faTimesCircle} />
+          </div>
+          <div className="alertTitle">
+            <div className="alertIcon">
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+            </div>
+            Alert
+          </div>
+          <div className="alertContent">
+            Accept the Terms and Conditions to login
+          </div>
+        </div>
+      </>
+    );
+  };
 
   const handleLogin = () => {
     loginDetails.setAuth(true);
   };
 
   return (
-    <div className="login_page">
-      <div className="inner_login">
-        <div className="images_container">
-          {[img0, img1, img2, img3, img4, img5, img6, img7, img8].map(
-            (img, index) => (
-              <div
-                key={index}
-                className={`img ${index === activeIndex ? "active" : ""}`}
-              >
-                <img src={img} alt={`${index}`} />
+    <>
+      {isTermsCondition && <Alert />}
+      <div className="login_page">
+        <div className="inner_login">
+          <div className="images_container">
+            {[img0, img1, img2, img3, img4, img5, img6, img7, img8].map(
+              (img, index) => (
+                <div
+                  key={index}
+                  className={`img ${index === activeIndex ? "active" : ""}`}
+                >
+                  <img src={img} alt={`${index}`} />
+                </div>
+              )
+            )}
+          </div>
+          <div className="login_form">
+            <form action="#" className="loginForm">
+              <h1 className="form_title">Login Form</h1>
+              <div className="inputField">
+                <label htmlFor="name">
+                  <FontAwesomeIcon icon={faUserAlt} /> User Name :
+                </label>
+                <input type="text" placeholder="eg: John Doe" required />
               </div>
-            )
-          )}
-        </div>
-        <div className="login_form">
-          <form action="#" className="loginForm">
-            <h1 className="form_title">Login Form</h1>
-            <div className="inputField">
-              <label htmlFor="name">
-                <FontAwesomeIcon icon={faUserAlt} /> User Name :
-              </label>
-              <input type="text" placeholder="eg: John Doe" required />
-            </div>
-            <div className="inputField">
-              <label htmlFor="password">
-                <FontAwesomeIcon icon={faUnlockKeyhole} /> Password :
-              </label>
-              <input type="password" placeholder="eg: ********" required />
-            </div>
-            <div className="tmc">
-              <label htmlFor="tmc">
-                <input type="checkbox" name="tmc" id="tnc" /> Terms & Conditions
-              </label>
-              <label htmlFor="signup">
-                <a href="/signup" className="signupLink">
-                  Sign up
-                </a>
-              </label>
-            </div>
-            <button
-              type="submit"
-              className="submit_button"
-              onClick={handleLogin}
-            >
-              <FontAwesomeIcon icon={faUserCheck} /> Login
-            </button>
-          </form>
+              <div className="inputField">
+                <label htmlFor="password">
+                  <FontAwesomeIcon icon={faUnlockKeyhole} /> Password :
+                </label>
+                <input type="password" placeholder="eg: ********" required />
+              </div>
+              <div className="tmc">
+                <label htmlFor="tmc">
+                  <input type="checkbox" name="tmc" id="tnc" /> Terms &
+                  Conditions
+                </label>
+                <label htmlFor="signup">
+                  <a href="/signup" className="signupLink">
+                    Sign up
+                  </a>
+                </label>
+              </div>
+              <button
+                type="submit"
+                className="submit_button"
+                onClick={handleLogin}
+              >
+                <FontAwesomeIcon icon={faUserCheck} /> Login
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
